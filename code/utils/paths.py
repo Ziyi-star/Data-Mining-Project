3e712276563de1a8b873064a5210039cd84cffce
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-07 13:24:32 +0200

    Add the Path file

---
+++ code/utils/paths.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+DATA_PATH = Path('..', 'data')
+LOGS_PATH = Path('..', 'logs')
+PREDICITIONS_PATH = Path('..', 'predictions')
+SCORES_PATH = Path('..', 'scores')
+PLOTS_PATH = Path('..', 'plots')
+MODELS_PATH = Path('..', 'saved_models')
