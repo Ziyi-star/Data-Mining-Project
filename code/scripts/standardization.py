a0fffd8c3310b37855614b2d20948cf63c9fdc9d
Ziyi Liu <ziyi.liu.0503@gmail.com>
2023-06-08 10:11:06 +0200

    path not working for 2 for loops

--- code/scripts/standardization.py
+++ code/scripts/standardization.py
@@ -4,17 +4,16 @@ import numpy as np
 import csv
 
 
-def standisierung(csv_data, new_standardzialsierung_data_path):
+def standardization(csv_data, new_stan_file_path):
     # Load the data
     data = pd.read_csv(csv_data)
     # Standardize the features
     scaler = StandardScaler()
     X = scaler.fit_transform(data)
-    print(X)
 
     # CSV-Datei öffnen
-    with open(new_standardzialsierung_data_path, 'w', newline='') as csv_file:
-        writer = csv.writer(new_standardzialsierung_data_path)
+    with open(new_stan_file_path, 'w', newline='') as csv_file:
+        writer = csv.writer(new_stan_file_path)
 
         # Daten aus JSON in CSV schreiben
         for item in data:
